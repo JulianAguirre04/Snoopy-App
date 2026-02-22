fb340258e916f5814fe054a6a3c94a8de11f9099
Julian <Julian.aguirre562@gmail.com>
2026-02-21 22:01:19 -0700

    small bug fixes

--- renderer.js
+++ renderer.js
@@ -3,14 +3,14 @@
 const messages = ["Hi!!!","I missed you!","test 3","wanna know who did 911","Ig I'm working now!"]
 
 // The DOM elements ;)
-const button = document.getElementById('new_messages') // button ID
-const messageEl = document.getElementById('element') // message ID
+const button = document.getElementById('new-message') // button ID
+const messageEl = document.getElementById('messages') // message ID
 
 // random funciton
 function randomFrom(arr) {
-    return arr[Math.floor(math.random() * arr.length)]
+    return arr[Math.floor(Math.random() * arr.length)]
 }
 
 button.addEventListener('click', (e) => {
-    messageEl.textElement = randomFrom(messages) // button listener to activate my messages
+    messageEl.textContent = randomFrom(messages) // button listener to activate my messages
 })
