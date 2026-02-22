1ca16d934b3e6c541aa3c3ba4b81e118248dac7b
Julian <Julian.aguirre562@gmail.com>
2026-02-21 22:40:15 -0700

    setting up electron window

--- mainSnoopy.js
+++ mainSnoopy.js
@@ -1,1 +1,21 @@
 // contains button functionality, message logic and animations
+const {app, BrowserWindow } = require('electron')
+const path = require('path') // setting up bas requirments and paths
+
+function createWindow () {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            //preload: path.join(__dirname, 'mainSnoopy.js'),
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    })
+
+    //win.loadFile('snoopIndex.html')
+    win.loadFile(path.join(__dirname, 'snoopyIndex.html')) // makes window load up the snoopy html file
+    win.webContents.openDevTools() // shows console errors
+}
+// makes sure to run when everything is ready, prevents jumping the gun
+app.whenReady().then(createWindow)
